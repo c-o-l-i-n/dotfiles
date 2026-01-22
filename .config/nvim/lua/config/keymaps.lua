4cd93133e1b1245a9fb2b7fe2149d2bc2a559f60
Colin Williams <40863449+c-o-l-i-n@users.noreply.github.com>
2026-01-21 22:24:33 -0500

    nvim: Add keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- center cursor on page after scrolling with <C-d> and <C-u>
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- exit input mode with jj to avoid reacing for the <Esc> key that's soooo far away
+vim.keymap.set("i", "jj", "<Esc>")
